03d28622f116020eeaaa1e4563d92c728e3c0129
Ada <Ada0610wf@163.com>
2019-07-12 10:43:56 +0800

    ak01

---
+++ base/test01.py
@@ -0,0 +1,14 @@
+# from selenium.webdriver.common.by import By
+#
+# # loc = [By.ID, "id1"]
+# loc = (By.ID, "id1")
+# print(loc[0],loc[1])
+# print("--" * 20)
+# print(*loc)
+# import page
+#
+# print(page.name)
+
+# loc = (1,2)
+loc = 1,2
+print(type(loc))
